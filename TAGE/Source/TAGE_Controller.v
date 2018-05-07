4020dc19a3689924a65dbcd6d49acfb607cdda66
Royan <royan.tuscano@gmail.com>
2018-05-07 12:30:48 -0700

    Update TAGE_Controller.v

--- TAGE/Source/TAGE_Controller.v
+++ TAGE/Source/TAGE_Controller.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/13/2017 03:21:16 PM
-// Design Name: 
-// Module Name: Comparator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
+//This is the module that is responsible for controlling the operations of the TAGE predictor, you can pipeline the operations for higher speedup
 module TAGE_Controller(CLK,reset,
 						index_tag_enable,instruction_inc_en,table_read_en, update_enable, update_predictor_enable);
 
