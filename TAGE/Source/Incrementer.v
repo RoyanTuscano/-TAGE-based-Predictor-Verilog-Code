bc4623fc5fcaa51c936bd70f530596ba749761d6
Royan <royan.tuscano@gmail.com>
2018-05-07 12:26:18 -0700

    Update Incrementer.v

--- TAGE/Source/Incrementer.v
+++ TAGE/Source/Incrementer.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/06/2017 10:02:01 PM
-// Design Name: 
-// Module Name: Incrementor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Incrementer(Clk, Rst, en, InstructionNumber_in, InstructionNumber_out);
     parameter TRAINING_DATA_SIZE = 256;
