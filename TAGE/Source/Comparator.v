e43c58dfd12d29754086578400df130165cf05a3
Royan <royan.tuscano@gmail.com>
2018-05-07 12:21:15 -0700

    Update Comparator.v

--- TAGE/Source/Comparator.v
+++ TAGE/Source/Comparator.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/13/2017 03:21:16 PM
-// Design Name: 
-// Module Name: Comparator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module comparator(clk,reset, a, b, eq);
     parameter DATAWIDTH = 1;
